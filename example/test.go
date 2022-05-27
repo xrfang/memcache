24f4a3d6806938ee99c642b857dbb8c308414c1e
larch <zhangsonglch@163.com>
2022-05-27 10:06:16 +0800

    更新测试案例

--- example/test.go
+++ example/test.go
@@ -8,16 +8,16 @@ import (
 )
 
 func main() {
-	cache := memorycache.InitCache(memorycache.CacheOption{MaxItems: 100, Expire: 1 * time.Hour})
+	cache := memorycache.New(&memorycache.CacheOption{})
 	fmt.Println("设置值", "zhangsan")
-	cache.Add("key1", "zhangsan", 5*time.Second)
+	cache.Set("key1", "zhangsan", 2*time.Second)
 	if it, ok := cache.Get("key1"); ok {
 		fmt.Printf("#1 取到值 %v\n", it)
 	} else {
 		fmt.Println("获取值失败")
 	}
-	fmt.Println("sleep 10s")
-	time.Sleep(10 * time.Second)
+	fmt.Println("sleep 5s")
+	time.Sleep(5 * time.Second)
 	if it, ok := cache.Get("key1"); ok {
 		fmt.Printf("#2 取到值 %v\n", it)
 	} else {
